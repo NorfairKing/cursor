2b220d9c8cacf7a79f5b24c362e7a5c1a49f01a9
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2018-08-28 15:16:40 +0200

    made the map cursor generator more maintainable

--- cursor-gen/src/Cursor/Map/Gen.hs
+++ cursor-gen/src/Cursor/Map/Gen.hs
@@ -5,8 +5,6 @@ module Cursor.Map.Gen
     (
     ) where
 
-import Test.QuickCheck
-
 import Data.GenValidity
 import Data.GenValidity.Containers ()
 
@@ -18,4 +16,4 @@ import Cursor.NonEmpty.Gen ()
 instance (GenUnchecked k, GenUnchecked v) => GenUnchecked (MapCursor k v)
 
 instance (GenValid k, GenValid v) => GenValid (MapCursor k v) where
-    genValid = (MapCursor <$> genValid) `suchThat` isValid
+    genValid = genValidStructurally
