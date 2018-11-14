22ae013d022870416f359df81633cb08d9c595da
Tom Sydney Kerckhove <syd@fpcomplete.com>
2018-11-14 16:29:29 +0100

    A nice way to render a nonempty list cursor

--- cursor/src/Cursor/List/NonEmpty.hs
+++ cursor/src/Cursor/List/NonEmpty.hs
@@ -31,6 +31,7 @@ module Cursor.List.NonEmpty
     , nonEmptyCursorDeleteElem
     , nonEmptyCursorSearch
     , nonEmptyCursorSelectOrAdd
+    , renderNonEmptyCursor
     , nonemptyPrepend
     , nonemptyAppend
     ) where
@@ -286,6 +287,10 @@ nonEmptyCursorSelectOrAdd f g p a nec =
     case nonEmptyCursorSearch f g p nec of
         Nothing -> nonEmptyCursorAppendAndSelect f a nec
         Just nec' -> nec'
+
+renderNonEmptyCursor :: ([b] -> a -> [b] -> c) -> NonEmptyCursor a b -> c
+renderNonEmptyCursor f NonEmptyCursor {..} =
+    f (reverse nonEmptyCursorPrev) nonEmptyCursorCurrent nonEmptyCursorNext
 
 nonemptyPrepend :: [a] -> NonEmpty a -> NonEmpty a
 nonemptyPrepend ls ne = foldr (<|) ne ls
