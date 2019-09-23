f0745011836b7da68739f9b82555d3ea47be0f5e
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-09-23 11:26:39 +0200

    another test

--- cursor-gen/test/Cursor/TextFieldSpec.hs
+++ cursor-gen/test/Cursor/TextFieldSpec.hs
@@ -30,20 +30,20 @@ spec :: Spec
 spec = do
   eqSpecOnValid @TextFieldCursor
   genValidSpec @TextFieldCursor
-  describe "makeTextFieldCursor" $
+  describe "makeTextFieldCursor" $ do
+    it "fails on \"\\n\"" $ shouldBeValid $ makeTextFieldCursor "\n"
     it "produces valid cursors" $ producesValidsOnValids makeTextFieldCursor
   describe "makeTextFieldCursorWithSelection" $ do
-    it "produces valid cursors" $
-      producesValidsOnValids3 makeTextFieldCursorWithSelection
-    it
-      "is the inverse of rebuildTextFieldCursor when using the current selection" $
+    it "fails on \"\\n\"" $
+      forAllValid $ \x ->
+        forAllValid $ \y -> shouldBeValid $ makeTextFieldCursorWithSelection x y "\n"
+    it "produces valid cursors" $ producesValidsOnValids3 makeTextFieldCursorWithSelection
+    it "is the inverse of rebuildTextFieldCursor when using the current selection" $
       forAllValid $ \tfc -> do
         let (x, y) = textFieldCursorSelection tfc
             t = rebuildTextFieldCursor tfc
         case makeTextFieldCursorWithSelection x y t of
-          Nothing ->
-            expectationFailure
-              "makeTextFieldCursorWithSelection should not have failed."
+          Nothing -> expectationFailure "makeTextFieldCursorWithSelection should not have failed."
           Just tfc' ->
             unless (tfc' == tfc) $
             expectationFailure $
@@ -58,21 +58,18 @@ spec = do
               , show t
               ]
   describe "rebuildTextFieldCursorLines" $ do
-    it "produces valid lists" $
-      producesValidsOnValids rebuildTextFieldCursorLines
+    it "produces valid lists" $ producesValidsOnValids rebuildTextFieldCursorLines
     it "produces texts without newlines" $
       forAllValid $ \tfc -> do
         let ls = NE.toList $ rebuildTextFieldCursorLines tfc
         unless (all (T.all (/= '\n')) ls) $
           expectationFailure $
-          unlines $
-          "Some of the following lines contain a newline:" : map show ls
+          unlines $ "Some of the following lines contain a newline:" : map show ls
   describe "rebuildTextFieldCursor" $ do
     it "produces valid texts" $ producesValidsOnValids rebuildTextFieldCursor
     it "is the inverse of makeTextFieldCursor for integers" $
       inverseFunctionsOnValid makeTextFieldCursor rebuildTextFieldCursor
-    it
-      "is the inverse of makeTextFieldCursorWithSelection for integers, for any index" $
+    it "is the inverse of makeTextFieldCursorWithSelection for integers, for any index" $
       forAllValid $ \x ->
         forAllValid $ \y ->
           inverseFunctionsIfFirstSucceedsOnValid
@@ -80,43 +77,34 @@ spec = do
             rebuildTextFieldCursor
   describe "textFieldCursorSelection" $
     it "produces valid tuples" $ producesValidsOnValids textFieldCursorSelection
-  describe "emptyTextFieldCursor" $
-    it "is valid" $ shouldBeValid emptyTextFieldCursor
-  describe "nullTextFieldCursor" $
-    it "produces valid" $ producesValidsOnValids nullTextFieldCursor
+  describe "emptyTextFieldCursor" $ it "is valid" $ shouldBeValid emptyTextFieldCursor
+  describe "nullTextFieldCursor" $ it "produces valid" $ producesValidsOnValids nullTextFieldCursor
   describe "textFieldCursorSelectPrevLine" $ do
-    it "produces valid cursors" $
-      producesValidsOnValids textFieldCursorSelectPrevLine
+    it "produces valid cursors" $ producesValidsOnValids textFieldCursorSelectPrevLine
     it "is a movement" $ isMovementM textFieldCursorSelectPrevLine
     it "selects the previous line" pending
   describe "textFieldCursorSelectNextLine" $ do
-    it "produces valid cursors" $
-      producesValidsOnValids textFieldCursorSelectNextLine
+    it "produces valid cursors" $ producesValidsOnValids textFieldCursorSelectNextLine
     it "is a movement" $ isMovementM textFieldCursorSelectNextLine
     it "selects the next line" pending
   describe "textFieldCursorSelectFirstLine" $ do
-    it "produces valid cursors" $
-      producesValidsOnValids textFieldCursorSelectFirstLine
+    it "produces valid cursors" $ producesValidsOnValids textFieldCursorSelectFirstLine
     it "is a movement" $ isMovement textFieldCursorSelectFirstLine
     it "is idempotent" $ idempotentOnValid textFieldCursorSelectFirstLine
     it "selects the first line" pending
   describe "textFieldCursorSelectLastLine" $ do
-    it "produces valid cursors" $
-      producesValidsOnValids textFieldCursorSelectLastLine
+    it "produces valid cursors" $ producesValidsOnValids textFieldCursorSelectLastLine
     it "is a movement" $ isMovement textFieldCursorSelectLastLine
     it "is idempotent" $ idempotentOnValid textFieldCursorSelectLastLine
     it "selects the last line" pending
   describe "textFieldCursorSelectPrevChar" $ do
-    it "produces valid cursors" $
-      producesValidsOnValids textFieldCursorSelectPrevChar
+    it "produces valid cursors" $ producesValidsOnValids textFieldCursorSelectPrevChar
     it "selects the previous character on the current line" pending
   describe "textFieldCursorSelectNextChar" $ do
-    it "produces valid cursors" $
-      producesValidsOnValids textFieldCursorSelectNextChar
+    it "produces valid cursors" $ producesValidsOnValids textFieldCursorSelectNextChar
     it "selects the previous character on the current line" pending
   describe "textFieldCursorIndexOnLine" $ do
-    it "produces valid indices" $
-      producesValidsOnValids textFieldCursorIndexOnLine
+    it "produces valid indices" $ producesValidsOnValids textFieldCursorIndexOnLine
     it "returns the index on the current line" pending
   describe "textFieldCursorSelectIndexOnLine" $ do
     it "produces valid cursors for any index" $
@@ -131,12 +119,10 @@ spec = do
       forAllValid $ \d -> producesValidsOnValids (textFieldCursorAppendChar d)
     it "inserts a character after the cursor on the currrent line" pending
   describe "textFieldCursorInsertNewline" $ do
-    it "produces valid cursors" $
-      producesValidsOnValids textFieldCursorInsertNewline
+    it "produces valid cursors" $ producesValidsOnValids textFieldCursorInsertNewline
     it "inserts a new line" pending
   describe "textFieldCursorAppendNewline" $ do
-    it "produces valid cursors" $
-      producesValidsOnValids textFieldCursorAppendNewline
+    it "produces valid cursors" $ producesValidsOnValids textFieldCursorAppendNewline
   describe "textFieldCursorRemove" $ do
     it "produces valid cursors" $ producesValidsOnValids textFieldCursorRemove
     it "removes empty text field cursor" $
@@ -148,12 +134,10 @@ spec = do
       textFieldCursorDelete emptyTextFieldCursor `shouldBe` Just Deleted
     it "deletes a character or a line" pending
   describe "textFieldCursorSelectStartOfLine" $ do
-    it "produces valid cursors" $
-      producesValidsOnValids textFieldCursorSelectStartOfLine
+    it "produces valid cursors" $ producesValidsOnValids textFieldCursorSelectStartOfLine
     it "selects the start of the current line" pending
   describe "textFieldCursorSelectEndOfLine" $ do
-    it "produces valid cursors" $
-      producesValidsOnValids textFieldCursorSelectEndOfLine
+    it "produces valid cursors" $ producesValidsOnValids textFieldCursorSelectEndOfLine
     it "selects the end of the current line" pending
 
 isMovementM :: (TextFieldCursor -> Maybe TextFieldCursor) -> Property
@@ -175,5 +159,4 @@ isMovementM func =
 
 isMovement :: (TextFieldCursor -> TextFieldCursor) -> Property
 isMovement func =
-  forAllValid $ \tfc ->
-    rebuildTextFieldCursor tfc `shouldBe` rebuildTextFieldCursor (func tfc)
+  forAllValid $ \tfc -> rebuildTextFieldCursor tfc `shouldBe` rebuildTextFieldCursor (func tfc)
