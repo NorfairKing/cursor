452e43cb466f8ac010da8318f84836e8ceca1efe
Tom Sydney Kerckhove <syd@cs-syd.eu>
2019-12-07 11:42:00 +0100

    pre-commit hooks via nix

--- default.nix
+++ default.nix
@@ -1,16 +1,19 @@
 let
-  pkgsv = import (import ./nix/nixpkgs.nix);
+  pkgsv = import ( import ./nix/nixpkgs.nix );
   pkgs' = pkgsv {};
-  validity-overlay = import (
-    (pkgs'.fetchFromGitHub (import ./nix/validity-version.nix)
-    + "/nix/overlay.nix")
-  );
-  pkgs = pkgsv {
-    overlays = [
-      validity-overlay
-      (import ./nix/gitignore-src.nix)
-      (import ./nix/overlay.nix)
-    ];
-    config.allowUnfree = true;
+  nix-pre-commit-hooks =
+    import (
+      builtins.fetchTarball "https://github.com/hercules-ci/nix-pre-commit-hooks/archive/5c3078ad58856ce22f883b5518879d27bfc59dd5.tar.gz"
+    );
+  pkgs = import ./nix/pkgs.nix;
+in
+  pkgs.cursorPackages // {
+  pre-commit-check = nix-pre-commit-hooks.run {
+    src = ./.;
+     hooks = {
+       hindent.enable = true;
+       canonix.enable = true;
+       hlint.enable = true;
+     };
   };
-in pkgs.cursorPackages
+}
---
+++ nix/pkgs.nix
@@ -0,0 +1,23 @@
+let
+  pkgsv = import ( import ./nixpkgs.nix );
+  pkgs' = pkgsv {};
+  validity-overlay =
+    import (
+      (
+        pkgs'.fetchFromGitHub (import ./validity-version.nix)
+    + "/nix/overlay.nix"
+      )
+    );
+  nix-pre-commit-hooks =
+    import (
+      builtins.fetchTarball "https://github.com/hercules-ci/nix-pre-commit-hooks/archive/a3bd860016653ab53ed49e2c4523e3e7297e58bb.tar.gz"
+    );
+in
+  pkgsv {
+    overlays =
+      [
+        validity-overlay
+        ( import ./gitignore-src.nix )
+        ( import ./overlay.nix )
+      ];
+  }
