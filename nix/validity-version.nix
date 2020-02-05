36dbdb1a38b4b16bd77ea7073e547a94ac71d067
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-02-05 22:04:55 +0100

    upgraded the validity version

--- nix/validity-version.nix
+++ nix/validity-version.nix
@@ -1,6 +1,6 @@
 {
   owner = "NorfairKing";
   repo = "validity";
-  rev = "a0fcd782138dac7200e6544c2f40208649ecb0e9";
-  sha256 = "sha256:1n7yp33a8n05n8w41c90r4xbpmnipnwj1kjawpmbi0ai04lzqv8b";
+  rev = "7f1d3d731062d4448aed306f4c5ddc5193f2a1e5";
+  sha256 = "sha256:0yi03b48xvqa0vl56ggjr1sfpfdpbi47fp4fsd9miibmr16wv7q9";
 }
